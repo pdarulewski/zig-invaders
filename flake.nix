1c81a131d666419c45f222959789273f78f9a440
Pawel Darulewski <pawel.darulewski@gmail.com>
2026-06-12 17:30:33 +0200

    feat: init

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "zig dev env";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.zig
+            pkgs.zig-zlint
+            pkgs.zls
+          ];
+        };
+      }
+    );
+}
